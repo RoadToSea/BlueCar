4296597101bc424c8b8c3bf97903610ea3a642fb
RoadToSea <3376714571@qq.com>
2025-01-14 11:01:35 +0800

    new commit

--- car/Core/Src/main.c
+++ car/Core/Src/main.c
@@ -96,7 +96,7 @@ int main(void)
   /* USER CODE BEGIN 2 */
 	CarInit();
 	Car* car = getCarInstance();
-	  /* USER CODE END 2 */
+  /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
